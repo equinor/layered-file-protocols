7e39c95e2ba37e5dc1db7171374461dc3d1e6342
Jørgen Kvalsvik <jokva@equinor.com>
2020-05-06 13:34:48 +0200

    Remove asserts on index.empty()
    The asserts were from a time where the index had all its state exposed
    (and was just a vector). With recent design changes, the need goes away
    as the integrity of the index is to guaranteed by the record_index class
    instead.

--- src/tapeimage.cpp
+++ src/tapeimage.cpp
@@ -97,8 +97,8 @@ public:
     void append(const header&) noexcept (false);
 
     iterator last() const noexcept (true);
-    using base::empty;
     std::size_t size() const noexcept (true);
+    bool empty() const noexcept (true);
     iterator begin() const noexcept (true);
 
     iterator::difference_type index_of(const iterator&) const noexcept (true);
@@ -330,12 +330,15 @@ void record_index::append(const header& h) noexcept (false) {
 }
 
 record_index::iterator record_index::last() const noexcept (true) {
-    assert(not this->empty());
     return std::prev(this->end());
 }
 
 std::size_t record_index::size() const noexcept (true) {
     return this->base::size() - 2;
+}
+
+bool record_index::empty() const noexcept (true) {
+    return this->size() == 0;
 }
 
 record_index::iterator record_index::begin() const noexcept (true) {
@@ -464,7 +467,6 @@ noexcept (false) {
 
 std::int64_t tapeimage::readinto(void* dst, std::int64_t len) noexcept (false) {
     assert(this->current.bytes_left() >= 0);
-    assert(not this->index.empty());
     std::int64_t bytes_read = 0;
 
     while (true) {
@@ -525,7 +527,6 @@ std::int64_t tapeimage::readinto(void* dst, std::int64_t len) noexcept (false) {
 
 // TODO: status instead of boolean?
 int tapeimage::eof() const noexcept (true) {
-    assert(not this->index.empty());
     // TODO: consider when this says record, but physical file is EOF
     // TODO: end-of-file is an _empty_ record, i.e. two consecutive tape marks
     return this->current->type == tapeimage::file;
@@ -652,7 +653,7 @@ void tapeimage::read_header_from_disk() noexcept (false) {
             this->recovery = LFP_PROTOCOL_TRYRECOVERY;
             head.prev = back2.next;
         }
-    } else if (this->recovery && !this->index.empty()) {
+    } else if (this->recovery and not this->index.empty()) {
         /*
          * In this case we have just two headers (A and B)
          * ------------------------
@@ -674,7 +675,6 @@ void tapeimage::read_header_from_disk() noexcept (false) {
 }
 
 void tapeimage::seek(std::int64_t n) noexcept (false) {
-    assert(not this->index.empty());
     assert(n >= 0);
 
     if (std::numeric_limits<std::uint32_t>::max() < n)
@@ -739,8 +739,6 @@ void tapeimage::seek(std::int64_t n) noexcept (false) {
 }
 
 std::int64_t tapeimage::tell() const noexcept (false) {
-    assert(not this->index.empty());
-
     const auto pos = this->index.index_of(this->current);
     return this->addr.logical(this->current.tell(), pos);
 }
