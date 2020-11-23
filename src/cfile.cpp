49cf26e1ae1a1a76e934046e531c950d10ed81e8
Alena Chaikouskaya <alec@equinor.com>
2020-11-23 17:34:00 +0100

    Learn how to copy correctly

--- src/cfile.cpp
+++ src/cfile.cpp
@@ -129,7 +129,7 @@ std::int64_t cfile::tell() const noexcept (false) {
     #ifdef HAS_FTELLO
         off = ftello(this->fp.get());
     #elif HAS_FTELLI64
-        off = ftelli64(this->fp.get());
+        off = _ftelli64(this->fp.get());
     #else
         static_assert(false, "neither ftello no _ftelli64 are available");
     #endif
